138ae6524fa75b558262f8381bf20104fb533f9b
Maiddog <maiddogsrl@gmail.com>
2017-02-06 00:27:06 -0600

    Update cartridge

--- RoughNES/cartridge.cpp
+++ RoughNES/cartridge.cpp
@@ -1,16 +1,38 @@
 #include "cartridge.h"
 
 #include <fstream>
-
-Cartridge::Cartridge() : m_mapper(0), m_battery(0), m_mirror(0)
-{
-  
-}
+#include <iterator>
 
 Cartridge::Cartridge(std::string filename)
 {
   std::ifstream ifs(filename, std::ios::binary);
 
+  if (!ifs.is_open())
+  {
+    throw std::invalid_argument("Could not open file");
+  }
+  
+  ifs.unsetf(std::ios::skipws);
 
+  ifs.seekg(0, std::ios::end);
+  auto filesize = static_cast<size_t>(ifs.tellg());
+  ifs.seekg(0, std::ios::beg);
+
+  std::vector<uint8_t> rom(filesize);
+
+  rom.insert(std::begin(rom),
+    std::istream_iterator<uint8_t>(ifs),
+    std::istream_iterator<uint8_t>());
+
+  m_header = NESHeader(rom);
+
+  auto prg_size = m_header.prg_pages() * PRGSize;
+  auto chr_size = m_header.chr_pages() * CHRSize;
+
+  m_prgrom.resize(prg_size);
+  m_chrrom.resize(chr_size);
+
+  std::copy(std::begin(rom) + 0x10, std::begin(rom) + 0x10 + prg_size, std::begin(m_prgrom));
+  std::copy(std::begin(rom) + 0x10 + prg_size, std::begin(rom) + 0x10 + prg_size + chr_size, std::begin(m_chrrom));
 }
 
--- RoughNES/cartridge.h
+++ RoughNES/cartridge.h
@@ -2,16 +2,20 @@
 
 #include <vector>
 
+#include "nes_header.h"
+
 class Cartridge
 {
+  static const size_t PRGSize = 0x4000;
+  static const size_t CHRSize = 0x2000;
+
   std::vector<uint8_t> m_prgrom;
   std::vector<uint8_t> m_chrrom;
   std::vector<uint8_t> m_sram;
-  uint8_t m_mapper;
-  uint8_t m_battery;
-  uint8_t m_mirror;
 
+  NESHeader m_header;
 public:
-  Cartridge();
   explicit Cartridge(std::string filename);
+
+  inline std::vector<uint8_t> prg_rom() const { return m_prgrom; }
 };
