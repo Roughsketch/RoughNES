8a446f4d966d1fe309bcc8ea980553a92dd2da1f
Maiddog <maiddogsrl@gmail.com>
2017-02-06 00:27:59 -0600

    Add header constructor

--- RoughNES/nes_header.cpp
+++ RoughNES/nes_header.cpp
@@ -2,5 +2,36 @@
 
 NESHeader::NESHeader(std::vector<uint8_t>& header)
 {
-  
+  if (header[0] != 'N' ||
+      header[1] != 'E' ||
+      header[2] != 'S' ||
+      header[3] != 0x1A)
+  {
+    throw std::invalid_argument("Not a valid NES ROM: Header is invalid.");
+  }
+
+  m_prg_pages = header[4];
+  m_chr_pages = header[5];
+
+  m_mapper_index = ((header[6] & 0xF0) >> 4) | (header[7] & 0xF0);
+
+  m_fourscreen = (header[6] & 0x08) > 0;
+  m_trainer = (header[6] & 0x04) > 0;
+  m_sram = (header[6] & 0x02) > 0;
+  m_mirror = (header[6] & 0x01) > 0;
+
+  m_playchoice = (header[7] & 0x02) > 0;
+  m_vs_unisystem = (header[7] & 0x01) > 0;
+
+  m_submapper = 0;
+
+  m_is_nes2 = ((header[7] & 0x0C) >> 2) == 2;
+
+  if (m_is_nes2)
+  {
+    m_prg_pages |= ((header[9] & 0x0F) << 8);
+    m_chr_pages |= ((header[9] >> 4) << 8);
+    m_mapper_index |= ((header[8] & 0x0F) << 8);
+    m_submapper = header[8] >> 4;
+  }
 }
--- RoughNES/nes_header.h
+++ RoughNES/nes_header.h
@@ -4,6 +4,22 @@
 
 class NESHeader
 {
+  bool m_is_nes2;
+  uint16_t m_prg_pages;
+  uint16_t m_chr_pages;
+  uint16_t m_mapper_index;
+  bool m_fourscreen;
+  bool m_trainer;
+  bool m_sram;
+  bool m_mirror;
+  bool m_playchoice;
+  bool m_vs_unisystem;
+
+  uint8_t m_submapper;
 public:
+  NESHeader(){};
   explicit NESHeader(std::vector<uint8_t>& header);
+
+  inline uint16_t prg_pages() const { return m_prg_pages; }
+  inline uint16_t chr_pages() const { return m_chr_pages; }
 };
